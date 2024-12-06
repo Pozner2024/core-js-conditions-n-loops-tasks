fdad846929f21b0381182ae1a2c5766d9a621a34
Natallia Pazniak <natalyua_nata_@mail.ru>
2024-12-06 16:22:50 +0300

    feat: Completed another function

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -237,8 +237,14 @@ function isPalindrome(str) {
  *  'qwerty', 'Q'     => -1
  *  'qwerty', 'p'     => -1
  */
-function getIndexOf(/* str, letter */) {
-  throw new Error('Not implemented');
+function getIndexOf(str, letter) {
+  for (let i = 0; i < str.length; i = +1) {
+    if (str[i] === letter) {
+      return i;
+    }
+  }
+
+  return -1;
 }
 
 /**
@@ -256,8 +262,20 @@ function getIndexOf(/* str, letter */) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  let absoluteNum = Math.abs(num);
+
+  while (absoluteNum > 0) {
+    const currentDigit = absoluteNum % 10;
+
+    if (currentDigit === digit) {
+      return true;
+    }
+
+    absoluteNum = Math.floor(absoluteNum / 10);
+  }
+
+  return digit === 0 && num === 0;
 }
 
 /**
